3001a9e2166e0b4d8d58f701bf7405b6a919e168
jelarson <jesslarson93@gmail.com>
2020-07-21 13:34:53 -0600

    material ui button and typography

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { css } from '@emotion/core'
-import { Button } from '@material-ui/core'
+import { Button, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  homeStyle: {
+    fontStyle: 'oblique',
+    fontSize: '30px',
+  },
+})
 
 export default function Home() {
+  const classes = useStyles()
+
   return (
     <div>
+      <Typography className={classes.homeStyle} color="primary" variant="h1">
+        Hello From Typography!
+      </Typography>
       <Button color="secondary" variant="outlined">
         Click me!
       </Button>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
 import App from './components/app'
+import theme from './components/theme'
 
 function main() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>,
     document.querySelector('.app-wrapper')
   )
 }
